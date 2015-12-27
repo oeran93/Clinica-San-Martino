23c32a79b295e366ab8e50e6fa21db32a7f9e13d
Nicola Pedretti <np.pedretti@gmail.com>
2015-12-27 07:17:00 -0600

    original init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">		
+
+	</head>
+
+	<body>
+
+		<script type="text/javascript" src="./js/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+	</body>
+
+</html>
