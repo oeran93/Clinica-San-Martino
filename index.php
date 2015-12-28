ed8c432800469a197a9b939da9f7a8e6a734e648
Nicola Pedretti <np.pedretti@gmail.com>
2015-12-28 03:34:48 -0600

    adding navigation bar and header section

--- index.php
+++ index.php
@@ -1,12 +1,59 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">		
-
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="./css/layout.css">		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<title>Clinica San Martino</title>
 	</head>
 
 	<body>
-
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<button type="button" class="navbar-toggle collapsed"
+				data-toggle="collapse" data-target="#collapsemenu">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="collapsemenu">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#"><span class="fa fa-location-arrow fa-lg"></span>Dove Siamo</a></li>
+						<li><a href="#"><span class="fa fa-hospital-o fa-lg"></span>Servizi</a></li>
+						<li><a href="#"><span class="fa fa-users fa-lg"></span>Contatti</a></li>
+					</ul>
+				</div>
+			</div>	
+		</nav>
+		<div id="header_bg_image">
+			<div class="container" id="header">
+				<div class="row">
+					<section class="col-xs-12 col-sm-6 col-md-3" id="header_description">
+						<h1>Headline</h1>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+						industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
+						and scrambled it to make a type specimen book. It has survived not only five centuries, but also the 
+						leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with...
+				</section>
+				</div>
+			</div>
+		</div>
+		<div id="where_bg_image">
+			<div class="container" id="where">
+				<div class="row">
+					<section class="col-xs-12 col-sm-6 col-md-3 col-md-offset-9" id="where_description">
+						<h1>Headline</h1>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+						industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
+						and scrambled it to make a type specimen book. It has survived not only five centuries, but also the 
+						leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with...
+				</section>
+				</div>
+			</div>
+		</div>
 		<script type="text/javascript" src="./js/jquery-2.1.4.min.js"></script>
 		<script type="text/javascript" src="./js/bootstrap.min.js"></script>
 	</body>
