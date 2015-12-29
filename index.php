a075d1e8196d14fd4bd57eba9c2061edca6f72b6
Nicola Pedretti <np.pedretti@gmail.com>
2015-12-29 02:24:30 -0600

    Set up structure for navbar, header, where, contacts, services. Problems with navbar in sm size. Have to shape links uniformly. Have to improve map style

--- index.php
+++ index.php
@@ -3,59 +3,172 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link href='https://fonts.googleapis.com/css?family=Antic+Slab' rel='stylesheet' type='text/css'>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="./css/layout.css">		
+		<link rel="stylesheet" type="text/css" href="./css/layout.css">	
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<title>Clinica San Martino</title>
 	</head>
 
 	<body>
-		<nav class="navbar navbar-default navbar-fixed-top">
-			<div class="container">
-				<button type="button" class="navbar-toggle collapsed"
-				data-toggle="collapse" data-target="#collapsemenu">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="collapsemenu">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#"><span class="fa fa-location-arrow fa-lg"></span>Dove Siamo</a></li>
-						<li><a href="#"><span class="fa fa-hospital-o fa-lg"></span>Servizi</a></li>
-						<li><a href="#"><span class="fa fa-users fa-lg"></span>Contatti</a></li>
-					</ul>
-				</div>
-			</div>	
-		</nav>
 		<div id="header_bg_image">
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+					<button type="button" class="navbar-toggle collapsed"
+					data-toggle="collapse" data-target="#collapsemenu">
+						<span class="fa fa-bars fa-2x"></span>
+					</button>
+					<div id="logo" class="navbar-left"><a href="#"><img class="img-responsive" src="./images/brand/logo_3.png"></a></div>
+					<div class="collapse navbar-collapse" id="collapsemenu">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#"><span class="fa fa-location-arrow fa-lg"></span>Dove Siamo</a></li>
+							<li><a href="#"><span class="fa fa-hospital-o fa-lg"></span>Servizi</a></li>
+							<li><a href="#"><span class="fa fa-users fa-lg"></span>Il Team</a></li>
+							<li><a href="#"><span class="fa fa-phone fa-lg"></span>Contatti</a></li>
+						</ul>
+					</div>
+				</div>	
+			</nav>
 			<div class="container" id="header">
 				<div class="row">
-					<section class="col-xs-12 col-sm-6 col-md-3" id="header_description">
-						<h1>Headline</h1>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
-						industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-						and scrambled it to make a type specimen book. It has survived not only five centuries, but also the 
-						leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with...
-				</section>
+					<section class="col-xs-12 col-sm-6" id="header_description">
+						<h2 class="text-capitalize">clinica "San Martino"</h2>
+						<h4 class="text-capitalize">il cuore delle alpi, il cuore della medicina</h4>
+					</section>
+				</div>
+			</div>
+		</div>
+		<div id="services_bg_image">
+			<div class="container" id="services">
+				<div class="row">
+					
+					<section class="text-capitalize col-xs-6 col-sm-3 services_info">
+						<img class=" img-thumbnail img-responsive"src="./images/services/1.jpg">
+						<h4>radiologia e servizi diagnostici</h4>
+						Per diagnostica per immagini -o imaging- si intendono le modalità di formazione delle immagini biomediche utilizzate a scopo diagnostico. Tra queste l’ecografia, la risonanza magnetica (RMN) e la Tomografia assiale computerizzata (TAC).
+					</section>
+					<section class="text-capitalize col-xs-6 col-sm-3 services_info">
+						<img class="img-thumbnail img-responsive"src="./images/services/2.jpg">
+						<h4>Ambulatori Specialistici</h4>
+						Scegli tra le specialità elencate: Dermatologia, Cadiologia, Endocrinologia, Medicina Interna, Osteopatia, Medicina del Sonno, Otorinolaringoiatria, Urologia e Andrologia, Ginecologia
+					</section>
+					<section class="text-capitalize col-xs-6 col-sm-3 services_info">
+						<img class="img-thumbnail img-responsive"src="./images/services/3.jpg">
+						<h4>Chirurgia Maxillo-Facciale</h4>
+						La chirurgia maxillo-facciale è la specialità medico-chirurgica che si occupa delle operazioni mirate alla cura di vari tipi di patologia –tra cui traumatismo, malformazione, neoplasia, infezione, degenerazione – e di problematiche estetico-funzionali dei denti, della bocca e della faccia.
+					</section>
+					<section class="text-capitalize col-xs-6 col-sm-3 services_info">
+						<img class="img-thumbnail img-responsive"src="./images/services/4.jpg">
+						<h4>Implantologia Dentale</h4>
+						Con il termine Implantologia si intende l’insieme di tecniche chirurgiche atte alla riabilitazione funzionale dei pazienti il cui apparato dentale deve essere tutto o in parte ripristinato.
+					</section>
+					<section class="hidden text-capitalize col-xs-6 col-sm-3 services_info">
+						<img class="img-thumbnail img-responsive"src="./images/services/1.jpg">
+						<h4>Odontoiatria e Ortodonzia</h4>
+					</div>
+					</section>
+				</div>
+			</div>
+		</div>
+		<div id="contacts_bg_image">
+			<div class="container" id="contacts">
+				<div class="row">
+					<section class="col-xs-7 col-sm-5" id="contacts_info">
+						<div class="row contacts_info">
+							<div class="col-xs-2">
+								<span class="fa fa-phone-square fa-3x"></span>
+							</div>
+							<div class="col-xs-10">
+								+39 0341 1695111
+							</div>
+						</div>
+						<div class="row contacts_info">
+							<div class="col-xs-2">
+								<span class="fa fa-paper-plane fa-3x"></span>
+							</div>
+							<div class="col-xs-10">
+								info@clinicasmartino.it
+							</div>
+						</div>
+						<div class="row contacts_info">
+							<div class="col-xs-2">
+								<span class="fa fa-facebook-official fa-3x"></span>
+							</div>
+							<div class="col-xs-10">
+								<a href="https://www.facebook.com/clinicasmartinomalgrate/"> Visita La Nostra Pagina Facebook</a>
+							</div>
+						</div>
+					</section>
+					<section class="col-xs-5 col-sm-7" id="contacts_photo">
+						<img class="img-responsive img-rounded img-thumbnail" src="./images/contact/1.jpg">
+					</section>
 				</div>
 			</div>
 		</div>
 		<div id="where_bg_image">
 			<div class="container" id="where">
 				<div class="row">
-					<section class="col-xs-12 col-sm-6 col-md-3 col-md-offset-9" id="where_description">
-						<h1>Headline</h1>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
-						industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-						and scrambled it to make a type specimen book. It has survived not only five centuries, but also the 
-						leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with...
-				</section>
+					<section class="col-xs-12 col-sm-12 col-md-7" id="where_map">
+						<div id="map"></div>
+					</section>
+					<section class="col-xs-12 col-sm-12 col-md-5" id="where_info">
+						<div class="row where_info">
+							<section class="col-xs-2">
+								<span class="fa fa-bus fa-3x">
+							</section>
+							<section class="col-xs-10">
+								La clinica è facilmente raggiungibile dagli autobus: Linea Lecco n° 4 fermata Via Stabilini Lecco trasporti Linea n° c 40 fermata via provinciale OBI.
+							</section>
+						</div>
+						<div class="row where_info">
+							<section class="col-xs-2">
+								<span class="fa fa-plane fa-3x">
+							</section>
+							<section class="col-xs-10">
+								Aeroporto di Milano Linate, Aeroporto di Milano Malpensa, Aeroporto di Bergamo Orio al serio. Servizio di transfer in auto, Servizio di transfer elicottero.
+							</section>
+						</div>
+						<div class="row where_info">
+							<section class="col-xs-2">
+								<span class="fa fa-train fa-3x">
+							</section>
+							<section class="col-xs-10">
+								Stazione ferroviaria di Lecco servizio navetta, autobus c 40, taxi
+							</section>
+						</div>
+						<div class="row where_info">
+							<section class="col-xs-2">
+								<span class="fa fa-car fa-3x">
+							</section>
+							<section class="col-xs-10">
+								Parcheggio gratuito per pazienti e visitatori
+							</section>
+						</div>
+						<div class="row where_info">
+							<section class="col-xs-2">
+								<span class="fa fa-wheelchair fa-3x">
+							</section>
+							<section class="col-xs-10">
+								Ingresso e parcheggio prioritario per disabili e mamme in attesa in via Paradiso
+							</section>
+						</div>
+					</section>
 				</div>
 			</div>
 		</div>
 		<script type="text/javascript" src="./js/jquery-2.1.4.min.js"></script>
 		<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="./js/layout.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLzVV02U5Y6GHXWV-wz9hNOuFpfSJWtUk&callback=initMap"
+        async defer></script>
 	</body>
 
 </html>
+
+<!-- <section class="col-sm-6 col-md-4 hidden-xs" id="header_description">
+						<h2 class="text-capitalize">eccellenza italiana, nel cuore delle alpi</h2>
+						
+						La Clinica San Martino è situata a Malgrate, in provincia di Lecco sulla parte terminale del lago di Como, proprio dirimpetto a Lecco. I 4.324 abitanti di Malgrate sono solo una parte della popolazione totale della provincia di Lecco che conta in totale 340.192 abitanti distribuiti su  un territorio di 814 kmq...
+						<a href="#">Continua a Leggere</a>
+					</section> -->
